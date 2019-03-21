d36a137d23be919fb8d7b7f4ad0d5777adaf067f
robertotambunan <robertotambunan14@gmail.com>
2019-03-21 16:23:27 +0700

    my first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/DataDog/datadog-go/statsd"
+)
+
+type ()
+
+func main() {
+	c, err := statsd.New("ipdatadog")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// prefix every metric with the app name
+	c.Namespace = "test."
+	url := "http://yourexample.com/path?yourparameters=value"
+	client := &http.Client{}
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatal("Get: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		c.Tags = append(c.Tags, "failed")
+		log.Println("Success")
+		err = c.Count("service.count.nakama.academy", 1, nil, 1)
+		return
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("ReadAll: ", err)
+		return
+	}
+
+	log.Println(string(bodyBytes))
+	log.Println("Success")
+	c.Tags = append(c.Tags, "success")
+	err = c.Count("service.count.nakama.academy", 1, nil, 1)
+}
